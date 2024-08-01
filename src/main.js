f28858072e98c2c769c67c029e5f14fb35adcd9e
jasmirmd <jsmrmdn23@gmail.com>
2024-08-01 10:07:55 -0600

    minor changes

--- src/main.js
+++ src/main.js
@@ -3,8 +3,3 @@ import './base.css';
 import Default from './js/default';
 
 document.addEventListener('DOMContentLoaded', Default);
-
-// this to avoid page reload after pressing enter in input
-window.addEventListener('keydown', (e) => {
-  if(e.key === 'Enter') e.preventDefault();
-});
